387e905bcc6af9ab5a8377ef9bd8a9866b26cb4a
kyungjinleelee <lv_jin@naver.com>
2025-07-20 22:23:06 +0900

    init #1: initialize spring boot backend project

---
+++ backend/src/main/java/com/phonebid/app/PhonebidApplication.java
@@ -0,0 +1,13 @@
+package com.phonebid.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebidApplication.class, args);
+	}
+
+}
