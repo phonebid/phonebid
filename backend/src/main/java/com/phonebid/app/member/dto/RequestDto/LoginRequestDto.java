33f8c1153d99e0d2d8cce45e83ced413afa85c54
kyungjinleelee <lv_jin@naver.com>
2025-07-30 23:45:30 +0900

    feat #10: implement login service with JWT token generation

--- backend/src/main/java/com/phonebid/app/member/dto/RequestDto/LoginRequestDto.java
+++ backend/src/main/java/com/phonebid/app/member/dto/RequestDto/LoginRequestDto.java
@@ -1,13 +1,24 @@
 package com.phonebid.app.member.dto.RequestDto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+/**
+ * 로그인 요청 DTO
+ * 사용자 로그인을 위한 요청 데이터를 담는 클래스
+ */
 @Getter
-@Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
+    
+    @NotBlank(message = "유저ID는 필수입니다")
     private String username;
+    
+    @NotBlank(message = "비밀번호는 필수입니다")
     private String password;
 }
