83a412019edefb0d1f73c70e72ff703843fd83a0
joo-chang <jooc0311@gmail.com>
2025-08-24 21:07:20 +0900

    chore #42 : scaffold features skeleton (auth, auction, quote, ranking) with subfolders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,9 @@
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import AppRouter from "app/router";
 
-import Layout from "components/layout/Layout";
-import LoginPage from "pages/LoginPage";
-import SignupPage from "pages/SignupPage";
-import AuthCallbackPage from "pages/AuthCallbackPage";
-import ConfettiTestPage from "@/pages/ConfettiPage";
 import { useAuthStore } from "store/authStore";
-import AuctionListPage from "./pages/AuctionListPage";
-import WeeklyRankingPage from "./pages/WeeklyRankingPage";
-import QuoteCreatePage from "./pages/QuoteCreatePage";
 import SWRProvider from "app/providers/SWRProvider";
 
 function App() {
@@ -24,46 +16,19 @@ function App() {
 
   return (
     <SWRProvider>
-      <Router>
-        <Routes>
-          {/* 전체화면 페이지들 (Layout 없음) */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/auth/callback" element={<AuthCallbackPage />} />
-          <Route path="/confetti" element={<ConfettiTestPage />} />
-          <Route path="/auctions/create" element={<QuoteCreatePage />} />
-
-          {/* Layout이 포함된 일반 페이지들 */}
-          <Route
-            path="/*"
-            element={
-              <Layout>
-                <Routes>
-                  <Route path="/" element={<WeeklyRankingPage />} />
-                  {/* 추후 추가될 라우트들 */}
-                  <Route path="/auctions" element={<AuctionListPage />} />
-
-                  {/* <Route path="/register" element={<RegisterPage />} /> */}
-                  {/* <Route path="/profile" element={<ProfilePage />} /> */}
-                </Routes>
-              </Layout>
-            }
-          />
-        </Routes>
-
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </Router>
+      <AppRouter />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </SWRProvider>
   );
 }
---
+++ frontend/src/app/router.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Layout from "components/layout/Layout";
+import LoginPage from "pages/LoginPage";
+import SignupPage from "pages/SignupPage";
+import AuthCallbackPage from "pages/AuthCallbackPage";
+import ConfettiTestPage from "pages/ConfettiPage";
+import AuctionListPage from "pages/AuctionListPage";
+import WeeklyRankingPage from "pages/WeeklyRankingPage";
+import QuoteCreatePage from "pages/QuoteCreatePage";
+
+export const AppRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* 전체화면 페이지들 (Layout 없음) */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/auth/callback" element={<AuthCallbackPage />} />
+        <Route path="/confetti" element={<ConfettiTestPage />} />
+        <Route path="/auctions/create" element={<QuoteCreatePage />} />
+
+        {/* Layout이 포함된 일반 페이지들 */}
+        <Route
+          path="/*"
+          element={
+            <Layout>
+              <Routes>
+                <Route path="/" element={<WeeklyRankingPage />} />
+                {/* 추후 추가될 라우트들 */}
+                <Route path="/auctions" element={<AuctionListPage />} />
+              </Routes>
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
+
+
