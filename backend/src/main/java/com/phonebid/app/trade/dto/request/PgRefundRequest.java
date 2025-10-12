7e1c0deb6bc14190976a8169f2eddb810a270a33
kyungjinleelee <lv_jin@naver.com>
2025-10-12 21:13:52 +0900

    fix #52 : Add validation annotation on PgRefundRequest Dto

--- backend/src/main/java/com/phonebid/app/trade/dto/request/PgRefundRequest.java
+++ backend/src/main/java/com/phonebid/app/trade/dto/request/PgRefundRequest.java
@@ -2,11 +2,18 @@ package com.phonebid.app.trade.dto.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Builder
 public class PgRefundRequest {
+    @NotBlank
     private String pgTid;          // 원본 거래 ID
+    @NotNull
+    @Min(1)
     private Integer refundAmount;   // 환불 금액
+    @NotBlank
     private String reason;         // 환불 사유
 }
