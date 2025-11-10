d74640f46dee1e4dc12f42555efc4832b8e94e9b
kyungjinleelee <lv_jin@naver.com>
2025-11-10 18:14:27 +0900

    chore #57 : Delete quote repository interface

--- backend/src/main/java/com/phonebid/app/auction/repository/QuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.phonebid.app.auction.repository;
-
-import com.phonebid.app.auction.domain.Quote;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuoteRepository extends JpaRepository<Quote, UUID> {
-    // TODO: 필요 시 Quote 관련 커스텀 쿼리를 추가하세요.
-}
