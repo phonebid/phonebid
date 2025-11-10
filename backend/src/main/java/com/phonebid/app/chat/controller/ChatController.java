7dc553ebe8eb8e427024118a6cdebba0eb47f11a
kyungjinleelee <lv_jin@naver.com>
2025-11-10 20:23:55 +0900

    fix #57 : Fix a fault that is broadcast with a response set to null

--- backend/src/main/java/com/phonebid/app/chat/controller/ChatController.java
+++ backend/src/main/java/com/phonebid/app/chat/controller/ChatController.java
@@ -8,17 +8,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
+import com.phonebid.app.chat.service.ChatMessageService;    
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final ChatMessageService chatMessageService;
 
     @MessageMapping("/chat/{chatRoomId}/send")
     public void sendMessage(@DestinationVariable String chatRoomId,
                             @Payload ChatMessageSendRequest request) {
-        ChatMessageResponse response = null;
+        ChatMessageResponse response = chatMessageService.sendMessage(request);
         messagingTemplate.convertAndSend("/topic/chat/" + chatRoomId, response);
     }
 }
